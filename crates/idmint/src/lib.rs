cbca9716615b75877025fd03ff138e4a8f960d8b
selrisu <myirisuchan@gmail.com>
2026-04-08 08:47:29 +0400

    fix(idmint): add cfg attributes

--- crates/idmint/src/lib.rs
+++ crates/idmint/src/lib.rs
@@ -1,8 +1,14 @@
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 #![no_std]
 
+#[cfg(feature = "heap")]
 mod heap;
+
+#[cfg(feature = "stack")]
 mod stack;
 
+#[cfg(feature = "heap")]
 pub use heap::*;
+
+#[cfg(feature = "stack")]
 pub use stack::*;
